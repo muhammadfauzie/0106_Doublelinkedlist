e4c39e8001a2fd877c948478d2611d25d71a2938
fauzie primarka <fauzieprimarka@gmail.com>
2025-05-15 17:30:21 +0700

    "Menambahkan logika penghapusan node dan pemberitahuan setelah penghapusan"

--- DoubleLinkedList2.cpp
+++ DoubleLinkedList2.cpp
@@ -116,7 +116,12 @@ public:
                 current->next->prev = current->prev;
             }
 
-        } 
+            //step 5: delete the node 
+            delete current;
+            cout << "Record with roll number" << rollNo << "delete" << endl;
+
+        }
     }
+
 
 };
