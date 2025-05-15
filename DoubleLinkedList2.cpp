44e775a04d514b7183035091eadad4c6ed3d37dd
fauzie primarka <fauzieprimarka@gmail.com>
2025-05-15 15:46:23 +0700

    membuat program modifier

--- DoubleLinkedList2.cpp
+++ DoubleLinkedList2.cpp
@@ -20,10 +20,8 @@ public:
     {
         START = NULL;
     }
-    void addNode()
-    {
-        int nim;
-        cout << "\nEnter the roll number of the student: ";
-        cin >> nim;
-    }
+ 
+
+   
+    
 };
