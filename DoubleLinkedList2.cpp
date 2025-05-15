01284728429480418e5ccd0bb4273d6e015c5284
fauzie primarka <fauzieprimarka@gmail.com>
2025-05-15 17:18:12 +0700

    Menambahkan logika penyisipan node di antara dua node pada list

--- DoubleLinkedList2.cpp
+++ DoubleLinkedList2.cpp
@@ -68,6 +68,11 @@ public:
          return;
         }
 
+         //step 9 : Insert between current and current->next
+        newNode->next = current->next; // step 9a: newNode.next = current.next
+        newNode->prev = current; // step 9b: newNode.prev = current
+
     }    
+
 
 };
